39e793f6e48bdc0205ba1183f48a6fe6a4aff765
Igor Guariroba <igor.guariroba@jsl.com.br>
2024-11-01 16:19:37 -0300

    refactor: Ajustando hateoas para esse controller.

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProductListRequest;
+use App\Http\Requests\ProductUpdateRequest;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -29,29 +30,7 @@ class ProductController extends Controller
             $products = $productsQuery->paginate($perPage, ['*'], 'page', $page);
 
             $products->getCollection()->transform(function ($product) {
-                return [
-                    'id' => $product->id,
-                    'name' => $product->name,
-                    'price' => $product->price,
-                    'description' => $product->description,
-                    'stock' => $product->stock,
-                    'created_at' => $product->created_at,
-                    'updated_at' => $product->updated_at,
-                    'links' => [
-                        [
-                            'rel' => 'self',
-                            'href' => route('products.show', ['product' => $product->id])
-                        ],
-                        [
-                            'rel' => 'update',
-                            'href' => route('products.update', ['product' => $product->id])
-                        ],
-                        [
-                            'rel' => 'delete',
-                            'href' => route('products.destroy', ['product' => $product->id])
-                        ]
-                    ]
-                ];
+                return $this->transformProduct($product);
             });
 
             return response()->json($products);
@@ -61,33 +40,73 @@ class ProductController extends Controller
         }
     }
 
-    public function show(Product $id): JsonResponse
+    public function show(Product $product): JsonResponse
     {
-        return response()->json($id);
+        try {
+            return response()->json($this->transformProduct($product));
+        } catch (\Exception $e) {
+            Log::error('Erro ao retornar o produto: ' . $e->getMessage());
+            return response()->json(['message' => 'Erro interno'], 500);
+        }
     }
 
     public function store(Request $request, Product $product): JsonResponse
     {
-        $product::create($request->all());
-        return response()->json($product, 201);
+        try {
+            $createdProduct = $product::create($request->all());
+            return response()->json($this->transformProduct($createdProduct), 201);
+        } catch (\Exception $e) {
+            Log::error('Erro ao criar o produto: ' . $e->getMessage());
+            return response()->json(['message' => 'Erro interno'], 500);
+        }
     }
 
-    public function update(Request $request, Product $id): JsonResponse
+    public function update(ProductUpdateRequest $request, Product $id): JsonResponse
     {
-        $request->validate([
-            'name' => 'required',
-            'price' => 'required',
-            'description' => 'required',
-            'stock' => 'required'
-        ]);
-
-        $id->update($request->all());
-        return response()->json($id);
+        try {
+            $id->update($request->all());
+            return response()->json($this->transformProduct($id));
+        } catch (\Exception $e) {
+            Log::error('Erro ao atualizar o produto: ' . $e->getMessage());
+            return response()->json(['message' => 'Erro interno'], 500);
+        }
     }
 
     public function destroy(Product $id): JsonResponse
     {
-        $id->delete();
-        return response()->json(null, 204);
+        try {
+            $id->delete();
+            return response()->json(['message' => 'Produto deletado com sucesso'], 204);
+        } catch (\Exception $e) {
+            Log::error('Erro ao deletar o produto: ' . $e->getMessage());
+            return response()->json(['message' => 'Erro interno'], 500);
+        }
+    }
+
+    private function transformProduct($product): array
+    {
+        return [
+            'id' => $product->id,
+            'name' => $product->name,
+            'price' => $product->price,
+            'description' => $product->description,
+            'stock' => $product->stock,
+            'created_at' => $product->created_at,
+            'updated_at' => $product->updated_at,
+            'links' => [
+                [
+                    'rel' => 'self',
+                    'href' => route('products.show', ['product' => $product->id])
+                ],
+                [
+                    'rel' => 'update',
+                    'href' => route('products.update', ['product' => $product->id])
+                ],
+                [
+                    'rel' => 'delete',
+                    'href' => route('products.destroy', ['product' => $product->id])
+                ]
+            ]
+        ];
     }
 }
